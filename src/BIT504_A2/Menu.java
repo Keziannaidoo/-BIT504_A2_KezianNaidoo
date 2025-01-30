4708857a82bcb1f8df1ed114198e563ab5a9ffcc
Kezian <kezian.naidoo@scrums.com>
2025-01-30 11:50:20 +0200

    feat: update comment

--- src/BIT504_A2/Menu.java
+++ src/BIT504_A2/Menu.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-	// Create single instance for file reader
+	// Create single instance
 	ReadFileData readFileData = ReadFileData.getInstance();
 
 	// Read data from files
